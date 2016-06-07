4001aa4adc4178ef719bce083b50ed83cccc1aa5
Klajdi Bejko <kbejko@gmail.com>
2016-06-07 13:09:23 -0400

    Update 2016-06-07T17:09:22.071Z

--- js/j.js
+++ js/j.js
@@ -1,17 +1,1 @@
-function genQuote() {
-    $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?", function(data){
-    $(".quote").text(data.quoteText)
-    $(".author").text(data.quoteAuthor)
-    var tweetQuote = "https://twitter.com/intent/tweet?text=" + data.quoteText + " - " + data.quoteAuthor
-    $("#tweet").on("click", function(){
-      $(this).attr("href", tweetQuote)
-    })
-  })
-}
-
-$(document).ready(function(){
-    genQuote()
-   $("#generateQuote").on("click", function(){
-      genQuote()
-  })
-})
+function genQuote(){$.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?",function(t){$(".quote").text(t.quoteText),$(".author").text(t.quoteAuthor);var e="https://twitter.com/intent/tweet?text="+t.quoteText+" - "+t.quoteAuthor;$("#tweet").on("click",function(){$(this).attr("href",e)})})}$(document).ready(function(){genQuote(),$("#generateQuote").on("click",function(){genQuote()})});
